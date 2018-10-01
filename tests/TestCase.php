c2637b7036ff08aeb31abbf5317a0276f26ac146
Christian Taylor <christianbtaylor@gmail.com>
2018-10-01 16:02:07 -0500

    Setup tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,24 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /*
+     * Signs in a user or creates a new one and signs it in.
+     *
+     * @param mixed $attributes User attributes or user model
+     */
+    protected function signIn($attributes = [])
+    {
+        $namespace = config('auth.providers.users.model');
+
+        if ($attributes instanceof $namespace) {
+            $this->actingAs($attributes);
+
+            return $this;
+        }
+
+        $this->actingAs(create($namespace, $attributes));
+
+        return $this;
+    }
 }
